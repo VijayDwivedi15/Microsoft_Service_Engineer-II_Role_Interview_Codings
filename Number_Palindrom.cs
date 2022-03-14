3fb090cafeaa0c24e0cd96dad2565bc3336a106b
vijaydwivedi125 <vijaydwivedi125@gmail.com>
2022-03-14 20:18:06 +0530

    Number Palindrome.

---
+++ Number_Palindrom.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Microsoft_Service_Engineer_II_Interview_Coding_Practice
+{
+    class Number_Palindrom
+    {
+        static void Main(string[] args)
+        {
+            int number, reverse, sum = 0, temp;
+            Console.Write("Enter the Number: ");
+            number = int.Parse(Console.ReadLine());
+            temp = number;
+            while (number > 0)
+            {
+                reverse = number % 10;
+                sum = (sum * 10) + reverse;
+                number = number / 10;
+            }
+            if (temp == sum)
+                Console.Write("Number is Palindrome.");
+            else
+                Console.Write("Number is not Palindrome");
+
+            Console.ReadLine();
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -6,24 +6,24 @@ namespace Microsoft_Service_Engineer_II_Interview_Coding_Practice
     //------------Program to check string is palindrom or not---------------
     class Program
     {
-        static void Main(string[] args)
-        {
-            string mystr, revstr = "";
-            Console.WriteLine("Enter a string");
-            mystr = Console.ReadLine();
-            for (int i = mystr.Length - 1; i >= 0; i--) //String Reverse  
-            {
-                revstr += mystr[i].ToString();
-            }
-            if (revstr == mystr) // Checking whether string is palindrome or not  
-            {
-                Console.WriteLine("String is Palindrome \n Entered String Was {0} and reverse string is {1}", mystr, revstr);
-            }
-            else
-            {
-                Console.WriteLine("String is not Palindrome \n Entered String Was {0} and reverse string is {1}", mystr, revstr);
-            }
-            Console.ReadKey();
-        }
+        //static void Main(string[] args)
+        //{
+        //    string mystr, revstr = "";
+        //    Console.WriteLine("Enter a string");
+        //    mystr = Console.ReadLine();
+        //    for (int i = mystr.Length - 1; i >= 0; i--) //String Reverse  
+        //    {
+        //        revstr += mystr[i].ToString();
+        //    }
+        //    if (revstr == mystr) // Checking whether string is palindrome or not  
+        //    {
+        //        Console.WriteLine("String is Palindrome \n Entered String Was {0} and reverse string is {1}", mystr, revstr);
+        //    }
+        //    else
+        //    {
+        //        Console.WriteLine("String is not Palindrome \n Entered String Was {0} and reverse string is {1}", mystr, revstr);
+        //    }
+        //    Console.ReadKey();
+        //}
     }
 }
