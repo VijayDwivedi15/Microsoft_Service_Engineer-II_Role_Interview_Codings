09fc738889a134486a2befdafb0fae71f3f6819b
vijaydwivedi125 <vijaydwivedi125@gmail.com>
2022-03-17 12:55:04 +0530

    odd or even number check

---
+++ OddorEven.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Microsoft_Service_Engineer_II_Interview_Coding_Practice
+{
+    class OddorEven
+    {
+        //public static string oddorEven(int num)
+        //{
+        //    if (num % 2 == 0)
+        //    {
+        //        return "Number is Even";
+        //    }
+        //    else
+        //    {
+        //        return "Number is Odd";
+        //    }
+        //}
+
+        //public static void Main()
+        //{
+        //    Console.WriteLine("Enter a Number :");
+        //    int number = int.Parse(Console.ReadLine());
+
+        //    Console.WriteLine("{0}", oddorEven(number));
+
+        //    Console.ReadLine();
+        //}
+
+
+    }
+}
--- Remove_Duplicates_Chars_from_String.cs
+++ Remove_Duplicates_Chars_from_String.cs
@@ -8,21 +8,21 @@ namespace Microsoft_Service_Engineer_II_Interview_Coding_Practice
 {
     class Remove_Duplicates_Chars_from_String
     {
-        public static void Main()
-        {
-            Console.WriteLine("Enter a string :");
-            string str = Console.ReadLine();
-            string result = string.Empty;
+        //public static void Main()
+        //{
+        //    Console.WriteLine("Enter a string :");
+        //    string str = Console.ReadLine();
+        //    string result = string.Empty;
 
-            for (int i = 0; i < str.Length; i++)
-            {
-                if (!result.Contains(str[i]))
-                {
-                    result += str[i];
-                }
-            }
-            Console.WriteLine(result);
-            Console.ReadLine();
-        }
+        //    for (int i = 0; i < str.Length; i++)
+        //    {
+        //        if (!result.Contains(str[i]))
+        //        {
+        //            result += str[i];
+        //        }
+        //    }
+        //    Console.WriteLine(result);
+        //    Console.ReadLine();
+        //}
     }
 }
