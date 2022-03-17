9f4e1ac0cfdd5b61cab9365cf7487ea97c3dd72a
vijaydwivedi125 <vijaydwivedi125@gmail.com>
2022-03-17 12:14:30 +0530

    duplicate chars from string

---
+++ Remove_Duplicates_Chars_from_String.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Microsoft_Service_Engineer_II_Interview_Coding_Practice
+{
+    class Remove_Duplicates_Chars_from_String
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Enter a string :");
+            string str = Console.ReadLine();
+            string result = string.Empty;
+
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (!result.Contains(str[i]))
+                {
+                    result += str[i];
+                }
+            }
+            Console.WriteLine(result);
+            Console.ReadLine();
+        }
+    }
+}
--- SumOfDigits.cs
+++ SumOfDigits.cs
@@ -8,21 +8,21 @@ namespace Microsoft_Service_Engineer_II_Interview_Coding_Practice
 {
     class SumOfDigits
     {
-        static void Main(string[] args)
-        {
-            Console.Write("Enter the Number : ");
-            int number = int.Parse(Console.ReadLine());
-            int sum = 0, reminder;
+        //static void Main(string[] args)
+        //{
+        //    Console.Write("Enter the Number : ");
+        //    int number = int.Parse(Console.ReadLine());
+        //    int sum = 0, reminder;
 
-            while (number > 0)
-            {
-                reminder = number % 10;
-                sum = sum + reminder;
-                number = number / 10;
-            }
+        //    while (number > 0)
+        //    {
+        //        reminder = number % 10;
+        //        sum = sum + reminder;
+        //        number = number / 10;
+        //    }
 
-            Console.WriteLine($"The Sum of Digits is : {sum}");
-            Console.ReadKey();
-        }
+        //    Console.WriteLine($"The Sum of Digits is : {sum}");
+        //    Console.ReadKey();
+        //}
     }
 }
