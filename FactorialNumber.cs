9f3398585d14bcf0449e7aa5ba59a0cd35c758b5
vijaydwivedi125 <vijaydwivedi125@gmail.com>
2022-03-17 14:38:03 +0530

    test code

--- FactorialNumber.cs
+++ FactorialNumber.cs
@@ -8,25 +8,25 @@ namespace Microsoft_Service_Engineer_II_Interview_Coding_Practice
 {
     class FactorialNumber
     {
-        public static int Factorial(int number)
-        {
-            int value = 1;
-            for (int i = 1; i <= number; i++)
-            {
-                value = value * i;
-            }
-            return value;
-        }
+        //public static int Factorial(int number)
+        //{
+        //    int value = 1;
+        //    for (int i = 1; i <= number; i++)
+        //    {
+        //        value = value * i;
+        //    }
+        //    return value;
+        //}
 
-        public static void Main()
-        {
-            Console.WriteLine("Enter a Number :");
-            int number = int.Parse(Console.ReadLine());
+        //public static void Main()
+        //{
+        //    Console.WriteLine("Enter a Number :");
+        //    int number = int.Parse(Console.ReadLine());
 
-            Console.WriteLine("Factorial of Number is {0} ", Factorial(number));
+        //    Console.WriteLine("Factorial of Number is {0} ", Factorial(number));
 
-            Console.ReadLine();
-        }
+        //    Console.ReadLine();
+        //}
 
     }
 }
---
+++ TestPage.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Microsoft_Service_Engineer_II_Interview_Coding_Practice
+{
+    class TestPage
+    {
+
+        //public static int AddNums(int x, int y)
+        //{
+        //    return x - (-y);
+        //}
+
+        public static int MulWithOutStarOperator(int x, int y)
+        {
+            int value = 0;
+            for (int z = 1; z <= y; z++)
+            {
+                value = value + x;
+            }
+            return value;
+        }
+
+        public static void Main()
+        {
+            Console.WriteLine("Enter First Number :");
+            int num1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter Second Number :");
+            int num2 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Sum of multi number is {0}", MulWithOutStarOperator(num1, num2));
+            Console.ReadLine();
+        }
+    }
+}
