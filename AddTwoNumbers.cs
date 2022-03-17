9fd41c8d05fd9c5ec323c3f23ef1128cb4e44fba
vijaydwivedi125 <vijaydwivedi125@gmail.com>
2022-03-17 11:14:48 +0530

    Sum of two numbers

---
+++ AddTwoNumbers.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Microsoft_Service_Engineer_II_Interview_Coding_Practice
+{
+    class AddTwoNumbers
+    {
+        public static int Sum(int num1, int num2)
+        {
+            int total;
+            total = num1 + num2;
+            return total;
+        }
+
+        public static void Main()
+        {
+
+            Console.Write("Enter a first number: ");
+            int n1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter a second number: ");
+            int n2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("\nThe sum of two numbers is : {0} \n", Sum(n1, n2));
+            Console.ReadLine();
+        }
+    }
+}
--- Prime_Number_or_Not.cs
+++ Prime_Number_or_Not.cs
@@ -8,29 +8,29 @@ namespace Microsoft_Service_Engineer_II_Interview_Coding_Practice
 {
     class Prime_Number_or_Not
     {
-        public static void Main()
-        {
+        //public static void Main()
+        //{
 
-            Console.WriteLine("Enter a number");
-            int n = Convert.ToInt32(Console.ReadLine());
-            int a = 0;
-            for (int i = 1; i <= n; i++)
-            {
-                if (n % i == 0)
-                {
-                    a++;
-                }
-            }
+        //    Console.WriteLine("Enter a number");
+        //    int n = Convert.ToInt32(Console.ReadLine());
+        //    int a = 0;
+        //    for (int i = 1; i <= n; i++)
+        //    {
+        //        if (n % i == 0)
+        //        {
+        //            a++;
+        //        }
+        //    }
 
-            if (a == 2)
-            {
-                Console.WriteLine("{0} is a Prime Number", n);
-            }
-            else
-            {
-                Console.WriteLine("{0} Not a Prime Number", n);
-            }
-            Console.ReadLine();
-        }
+        //    if (a == 2)
+        //    {
+        //        Console.WriteLine("{0} is a Prime Number", n);
+        //    }
+        //    else
+        //    {
+        //        Console.WriteLine("{0} Not a Prime Number", n);
+        //    }
+        //    Console.ReadLine();
+        //}
     }
 }
