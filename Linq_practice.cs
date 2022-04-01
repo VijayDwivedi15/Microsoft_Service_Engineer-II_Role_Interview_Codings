4fdd5a3767dc7616f7ab67a6a49e2566aa1a3d19
vijaydwivedi125 <vijaydwivedi125@gmail.com>
2022-04-01 22:20:12 +0530

    reverse a words of string

--- Linq_practice.cs
+++ Linq_practice.cs
@@ -8,30 +8,30 @@ namespace Microsoft_Service_Engineer_II_Interview_Coding_Practice
 {
     class Linq_practice
     {
-        public static void Main()
-        {
-            var val = new int[] {99, 35, 26, 87};
+        //public static void Main()
+        //{
+        //    var val = new int[] {99, 35, 26, 87};
 
-            var maxNum = val.Max(z => z);   // Getting max value
-            Console.WriteLine("Maximum Number: " + maxNum);
+        //    var maxNum = val.Max(z => z);   // Getting max value
+        //    Console.WriteLine("Maximum Number: " + maxNum);
 
-            var minNum = val.Min(z => z);   // Getting min value
-            Console.WriteLine("Minimum Number: " + minNum);
+        //    var minNum = val.Min(z => z);   // Getting min value
+        //    Console.WriteLine("Minimum Number: " + minNum);
 
-            var secondMax = val.OrderByDescending(z => z).Skip(1).First();  // Getting second max value
-            Console.WriteLine("Second Largest Number: " + secondMax);
+        //    var secondMax = val.OrderByDescending(z => z).Skip(1).First();  // Getting second max value
+        //    Console.WriteLine("Second Largest Number: " + secondMax);
 
-            var secondMin = val.OrderBy(z => z).Skip(1).First();  // Getting second Min value
-            Console.WriteLine("Second Smallest Number: " + secondMin);
+        //    var secondMin = val.OrderBy(z => z).Skip(1).First();  // Getting second Min value
+        //    Console.WriteLine("Second Smallest Number: " + secondMin);
 
-            var thirddMin = val.OrderBy(z => z).Skip(2).First();  // Getting Third Min value
-            Console.WriteLine("Third Smallest Number: " + thirddMin);
+        //    var thirddMin = val.OrderBy(z => z).Skip(2).First();  // Getting Third Min value
+        //    Console.WriteLine("Third Smallest Number: " + thirddMin);
 
-            var thirddMax = val.OrderByDescending(z => z).Skip(2).First();  // Getting Third Max value
-            Console.WriteLine("Third Largest Number: " + thirddMax);
+        //    var thirddMax = val.OrderByDescending(z => z).Skip(2).First();  // Getting Third Max value
+        //    Console.WriteLine("Third Largest Number: " + thirddMax);
 
-            Console.ReadLine();
+        //    Console.ReadLine();
 
-        }
+        //}
     }
 }
---
+++ Reversing_words_of_String.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace ConsoleApplication9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Eneter the String:");
+
+            string s = Console.ReadLine();
+            string[] a = s.Split(' ');
+            Array.Reverse(a);
+
+            Console.WriteLine("Reverse String is:");
+            for (int i = 0; i <= a.Length - 1; i++)
+            {
+
+                Console.Write(a[i] + "" + ' ');
+            }
+            Console.ReadKey();
+        }
+    }
+}
